7e305bd7b0decd8e6d347a0d9a15782f2cf0d4a5
Rodrigo <hl>
2024-05-08 00:50:58 -0600

    v3

--- MainResta.java
+++ MainResta.java
@@ -1,26 +1,31 @@
 import java.util.Scanner;
+
 public class MainResta {
     public static void main(String[] args) {
+        // Crear un objeto Scanner para leer la entrada del usuario
         Scanner scanner = new Scanner(System.in);
+        // Crear un objeto de tipo Resta
         Resta resta = new Resta();
 
+        // Solicitar al usuario que ingrese el primer número
         System.out.println("Ingresa el primer número: ");
         int a = scanner.nextInt();
         resta.setA(a);
 
+        // Solicitar al usuario que ingrese el segundo número
         System.out.println("Ingresa el segundo número: ");
         int b = scanner.nextInt();
         resta.setB(b);
 
+        // Realizar la resta
         resta.restar();
 
+        // Obtener el resultado de la resta
         int resultado = resta.getR();
         System.out.println("El resultado de la resta es " + resultado);
         System.out.println(resta.toString());
 
+        // Cerrar el Scanner
         scanner.close();
     }
 }
-
-    
-
--- Resta.java
+++ Resta.java
@@ -1,30 +1,38 @@
 public class Resta {
+    // Variables para los valores y el resultado de la resta
     private int a, b, r;
 
+    // Método para obtener el valor 'a'
     public int getA() {
         return a;
     }
 
+    // Método para establecer el valor 'a'
     public void setA(int a) {
         this.a = a;
     }
 
+    // Método para obtener el valor 'b'
     public int getB() {
         return b;
     }
 
+    // Método para establecer el valor 'b'
     public void setB(int b) {
         this.b = b;
     }
 
+    // Método para realizar la resta de 'a' y 'b'
     public void restar() {
         this.r = a - b;
     }
 
+    // Método para obtener el resultado de la resta
     public int getR() {
         return r;
     }
 
+    // Método toString para representar la información de la resta
     @Override
     public String toString() {
         return "Resta [a=" + a + ", b=" + b + ", r=" + r + "]";
